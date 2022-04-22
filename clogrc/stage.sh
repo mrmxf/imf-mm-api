016f56cf98e9df9e34d6ee4cd38baca1f9c29cf5
Bruce Devlin <bruce@mrmxf.com>
2022-04-22 15:39:15 +0000

    gitpod clog tweak

--- clogrc/stage.sh
+++ clogrc/stage.sh
@@ -1,6 +1,11 @@
 # usage> stage
 # short> execute stage.sh to build & upload {{REPO}} to staging
 # long>  execute stage.sh to build & upload {{REPO}} to staging. No other option needed. Edit script to configure upload.
+#   _            __                                                _
+#  (_)  _ __    / _|  ___   _ _   ___   __ _   ___   __ _   _ __  (_)
+#  | | | '  \  |  _| |___| | '_| / -_) / _` | |___| / _` | | '_ \ | |
+#  |_| |_|_|_| |_|         |_|   \___| \__, |       \__,_| | .__/ |_|
+#                                      |___/               |_|
 
 source $GITPOD_REPO_ROOT/clogrc/core/mm-core-inc.sh
 fnInfo "Project(${cH}$(basename $GITPOD_REPO_ROOT)${cT})$cF $(basename $0)"
